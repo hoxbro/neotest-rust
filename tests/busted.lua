6546e6f07fd85073e9232c4e682cd8ca69951bba
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-12-23 21:32:40 +0100

    ci: Update infrastructure (#2)

--- tests/busted.lua
+++ tests/busted.lua
@@ -1,23 +1,43 @@
-local cwd = vim.loop.cwd()
+local lazypath = vim.env.LAZY_STDPATH
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        "--depth=1",
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
 
-vim.opt.rtp:append(cwd)
-vim.opt.rtp:append(cwd .. "/deps/lazy.nvim")
+local root = vim.fn.fnamemodify(vim.env.LAZY_STDPATH, ":p")
+for _, name in ipairs({ "config", "data", "state", "cache" }) do
+    vim.env[("XDG_%s_HOME"):format(name:upper())] = root .. "/" .. name
+end
 
-require("lazy.minit").busted({
+local opts = require("lazy.minit").busted.setup({
     spec = {
         "nvim-lua/plenary.nvim",
         {
             "nvim-treesitter/nvim-treesitter",
-            config = function()
-                local configs = require("nvim-treesitter.configs")
-
-                configs.setup({
-                    ensure_installed = { "rust" },
-                    sync_install = true,
-                })
-            end,
+            branch = "master",
+            build = ":TSUpdate",
+            main = "nvim-treesitter.configs",
+            opts = { ensure_installed = { "rust" } },
         },
         "nvim-neotest/nvim-nio",
         "nvim-neotest/neotest",
     },
+    lockfile = "tests/lazy-lock.json",
 })
+
+vim.o.loadplugins = true
+require("lazy").setup(opts)
+
+if _G.arg[1] == "--update" then
+    require("lazy").update():wait()
+else
+    require("lazy.minit").busted.run()
+end
