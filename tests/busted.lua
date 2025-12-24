c981672a3c6181c60d6465f5b5fd2c1c70c4a784
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-12-24 12:19:17 +0100

    handle master branch for neovim 0.10

--- tests/busted.lua
+++ tests/busted.lua
@@ -1,3 +1,5 @@
+local language = "rust"
+
 local lazypath = vim.env.LAZY_STDPATH
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     vim.fn.system({
@@ -12,25 +14,43 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local spec = {
-    "nvim-lua/plenary.nvim",
-    {
+local treesitter_spec
+local lockfile
+if vim.fn.has("nvim-0.11") == 1 then
+    treesitter_spec = {
         "nvim-treesitter/nvim-treesitter",
-        main = "nvim-treesitter.configs",
         config = function(_, opts)
             require("nvim-treesitter").setup(opts)
             local installed = require("nvim-treesitter.config").get_installed()
-            if not vim.tbl_contains(installed, "rust") then
-                require("nvim-treesitter").install({ "rust" }):wait()
+            if not vim.tbl_contains(installed, language) then
+                require("nvim-treesitter").install({ language }):wait()
             end
         end,
-    },
-    "nvim-neotest/nvim-nio",
-    "nvim-neotest/neotest",
-}
+    }
+    lockfile = "tests/lazy-lock.json"
+else
+    treesitter_spec = {
+        "nvim-treesitter/nvim-treesitter",
+        branch = "master",
+        config = function(_, opts)
+            require("nvim-treesitter.configs").setup(opts)
+            local parsers = require("nvim-treesitter.parsers")
+            if not parsers.has_parser(language) then
+                vim.cmd("TSInstallSync " .. language)
+            end
+        end,
+    }
+    lockfile = "tests/lazy-lock-10.json"
+end
+
 local opts = {
-    spec = spec,
-    lockfile = "tests/lazy-lock.json",
+    spec = {
+        "nvim-lua/plenary.nvim",
+        treesitter_spec,
+        "nvim-neotest/nvim-nio",
+        "nvim-neotest/neotest",
+    },
+    lockfile = lockfile,
 }
 
 if _G.arg[1] == "--update" then
