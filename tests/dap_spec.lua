595d467d3fcd4621197505d73598e7f59eeeaf72
Simon Høxbro Hansen <hoxbro@protonmail.com>
2026-04-04 18:39:43 +0200

    move to vim.uv

--- tests/dap_spec.lua
+++ tests/dap_spec.lua
@@ -7,7 +7,7 @@ describe("get_test_binary", function()
     -- tests/testsuite/main.rs. We can only test that they match expected substrings
     -- and that the other modules resolve to their source binaries
     describe("for a simple-package", function()
-        local cwd = vim.loop.cwd()
+        local cwd = vim.uv.cwd()
         local root = cwd .. "/tests/data/simple-package"
 
         local lib_actual = dap.get_test_binary(root, root .. "/src/lib.rs")
@@ -93,7 +93,7 @@ describe("get_test_binary", function()
     end)
 
     describe("for a workspace", function()
-        local cwd = vim.loop.cwd()
+        local cwd = vim.uv.cwd()
         local root = cwd .. "/tests/data/workspace"
 
         async.it("returns the test binary for with_unit_tests/src/main.rs", function()
@@ -131,7 +131,7 @@ end)
 
 describe("translate_results", function()
     async.it("parses results with a single test suite in it", function()
-        local path = vim.loop.cwd() .. "/tests/data/simple-package/1"
+        local path = vim.uv.cwd() .. "/tests/data/simple-package/1"
 
         local results = dap.translate_results(path)
 
@@ -143,7 +143,7 @@ describe("translate_results", function()
     end)
 
     async.it("translates raw results with multiple test suites in it", function()
-        local path = vim.loop.cwd() .. "/tests/data/simple-package/3"
+        local path = vim.uv.cwd() .. "/tests/data/simple-package/3"
 
         local results = dap.translate_results(path)
 
--- tests/util_spec.lua
+++ tests/util_spec.lua
@@ -2,7 +2,7 @@ local async = require("nio.tests")
 local util = require("neotest-rust.util")
 
 describe("file_exists", function()
-    local cwd = vim.loop.cwd()
+    local cwd = vim.uv.cwd()
 
     async.it("returns true when the file exists", function()
         local path = cwd .. "/tests/data/simple-package/src/mymod/foo.rs"
